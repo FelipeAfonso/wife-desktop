b82a3a24ff0837a25c71b8de555dd1e2f3803be9
Felipe Munhoz Afonso <fmunhozafonso@gmail.com>
2023-07-27 17:20:43 -0300

    fix: removed random folder

--- nvim/nvim/lua/cod/carbon.lua
+++
@@ -1,9 +0,0 @@
-require('carbon').setup({
-    bang = true
-})
-
-vim.keymap.set("n", "<leader>t", '<cmd>Fcarbon<cr>')
-vim.keymap.set("n", "<leader>c", '<cmd>Fcarbon!<cr>')
-vim.keymap.set('n', '<leader>q', function ()
-        require('carbon.view').close_float()
-end)
